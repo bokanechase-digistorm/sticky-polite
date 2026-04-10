9e4dd44a9002249dd647e4b0666e006f7711c309
Bentley O'Kane-Chase <bentley.okanechase@digistorm.com>
2026-04-10 16:15:25 +1000

    Build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,8 @@
+declare const mountElement: (element: HTMLElement, scrollParent?: HTMLElement | null) => void;
 declare const initPoliteSticky: () => void;
 
 /**
  * Polite Sticky (Directional Reveal) Utility
  */
 
-export { initPoliteSticky as default };
+export { initPoliteSticky as default, mountElement };
